fe5b3153e3e21d17bd79279a95195f6bee14b520
mserhatyuksel <mserhat.yksl@gmail.com>
2021-11-16 11:51:41 +0300

    Added label, input, button and style

--- src/components/Inputs/UserInput/UserInput.jsx
+++ src/components/Inputs/UserInput/UserInput.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
-const UserInput = () => {
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "../../UI/Button";
+import styles from "./UserInput.module.css";
+const UserInput = ({ handleUser }) => {
+  const [value, setValue] = useState("");
   return (
-    <div>
-      user
+    <div className={styles.container}>
+      <label htmlFor="name">Please enter your name</label>
+      <input
+        type="text"
+        id="name"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Link to="/height" onClick={() => handleUser(value)}>
+        <Button>Next</Button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default UserInput
+export default UserInput;
