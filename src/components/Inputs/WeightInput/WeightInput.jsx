3e1b34dca4f60af64d84cba1b2b27496d2d3b2a2
mserhatyuksel <mserhat.yksl@gmail.com>
2021-11-16 13:28:23 +0300

    Added input, button and style

--- src/components/Inputs/WeightInput/WeightInput.jsx
+++ src/components/Inputs/WeightInput/WeightInput.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
-const WeightInput = () => {
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "../../UI/Button";
+import styles from "./WeightInput.module.css";
+const WeightInput = ({ handleWeight }) => {
+  const [value, setValue] = useState("");
   return (
-    <div>
-      weight
+    <div className={styles.container}>
+      <img src="/assets/images/weight.png" alt="" />
+      <div className={styles.input}>
+        <label htmlFor="weight">What is your weight?</label>
+        <input
+          type="number"
+          min="10"
+          max="300"
+          id="weight"
+          placeholder="kg"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Link to="/result" onClick={() => handleWeight(value)}>
+          <Button disabled={!value}>Next</Button>
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default WeightInput
+export default WeightInput;
