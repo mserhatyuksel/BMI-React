7d50835f74bb6e05a0f09855f5acf5e96bd8d253
mserhatyuksel <mserhat.yksl@gmail.com>
2021-11-16 11:52:17 +0300

    Remove links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,7 @@
-import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <div>
       <h1>BMI Calculator</h1>
-      <Link to="/">home</Link>
-      <Link to="/weight">weight</Link>
-      <Link to="/height">height</Link>
-      <Link to="/result">result</Link>
     </div>
   );
 };
