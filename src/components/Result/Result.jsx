94602160a8b585c55ccb6e28126c4663648a4601
mserhatyuksel <mserhat.yksl@gmail.com>
2021-11-16 14:05:52 +0300

    Added bmi function and images

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-
-const Result = () => {
+import React, { useEffect, useState } from "react";
+import styles from "./Result.module.css";
+const Result = ({ user, height, weight }) => {
+  const [bmi, setBmi] = useState();
+  useEffect(() => {
+    setBmi((weight / Math.pow(height / 100, 2)).toFixed(2));
+  }, []);
   return (
-    <div>
-      result
+    <div className={styles.container}>
+      <h3>
+        {user}'s BMI: {bmi}
+      </h3>
+      {bmi <= 18.4 ? (
+        <img className={styles.img} src="/assets/images/underweight.jpg" alt="" />
+      ) : bmi <= 24.9 ? (
+        <img className={styles.img} src="/assets/images/normal.jpg" alt="" />
+      ) : bmi <= 29.9 ? (
+        <img className={styles.img} src="/assets/images/excess_weight.png" alt="" />
+      ) : bmi <= 44.9 ? (
+        <img className={styles.img} src="/assets/images/obesity.jpg" alt="" />
+      ) : (
+        <img className={styles.img} src="/assets/images/extremely_obesity.jpg" alt="" />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Result
+export default Result;
