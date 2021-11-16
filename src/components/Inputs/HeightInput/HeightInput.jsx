7e9237b511a2f5c37706e55468cb28523ccd8e4e
mserhatyuksel <mserhat.yksl@gmail.com>
2021-11-16 13:19:18 +0300

    Added input, button and style

--- src/components/Inputs/HeightInput/HeightInput.jsx
+++ src/components/Inputs/HeightInput/HeightInput.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
-
-const HeightInput = () => {
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Button from "../../UI/Button";
+import styles from "./HeightInput.module.css";
+const HeightInput = ({ handleHeight }) => {
+  const [value, setValue] = useState("");
   return (
-    <div>
-      height
+    <div className={styles.container}>
+      <div className={styles.input}>
+        <label htmlFor="height">How tall are you?</label>
+        <input
+          type="number"
+          min="30"
+          max="220"
+          id="height"
+          placeholder="cm"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <Link to="/weight" onClick={() => handleHeight(value)}>
+          <Button disabled={!value}>Next</Button>
+        </Link>
+      </div>
+      <img src="/assets/images/height.png" alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default HeightInput
+export default HeightInput;
