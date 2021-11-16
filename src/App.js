680c2d3dff195927643c038fbb6832e2ef0371fa
mserhatyuksel <mserhat.yksl@gmail.com>
2021-11-16 11:52:46 +0300

    Added user state

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import WeightInput from "./components/Inputs/WeightInput";
 import HeightInput from "./components/Inputs/HeightInput";
 import Result from "./components/Result";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState("");
+  const handleUser = (name) => {
+    setUser(name);
+  };
   return (
     <div className="container">
       <Header />
       <Routes>
-        <Route path="/" exact element={<UserInput />} />
+        <Route path="/" exact element={<UserInput handleUser={handleUser} />} />
         <Route path="/weight" element={<WeightInput />} />
         <Route path="/height" element={<HeightInput />} />
-        <Route path="/result" element={<Result />} />
+        <Route path="/result" element={<Result user={user} />} />
       </Routes>
     </div>
   );
